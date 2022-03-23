0048ee4ee80fe8cb1eebb968301d163d8ba69612
Manuel Rodriguez <lolo.edinburgh@gmail.com>
2022-03-23 07:28:59 +0000

    adding in addToCart feature

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import './cart-item.styles.scss'
 
 const CartItem = ({cartItem}) => {
-
     return(
         <div className={`cart-item-container`}>
-            <img src={imageUrl} alt={name}/>
+            <img src={cartItem.imageUrl} alt={cartItem.name}/>
             <div className="item-details">
-                <span className={`name`}>{name}</span>
-                <span className={`price`}>{quantity} x {price}</span>
+                <span className={`name`}>{cartItem.name}</span>
+                <span className={`price`}>{cartItem.quantity} x {cartItem.price}</span>
 
             </div>
         </div>
